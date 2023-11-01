8ebf01d672e87e22840dfe199f53efe5c0be151d
Vanshika-Malik2004 <malikvanshika2004@gmail.com>
2023-11-01 18:45:53 +0530

    adding a navbar for dashboard

--- src/components/Progress.jsx
+++ src/components/Progress.jsx
@@ -1,11 +1,11 @@
-// import * as React from "react";
-// import CircularProgress from "@mui/material/CircularProgress";
-// import Box from "@mui/material/Box";
+import * as React from "react";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
 
-// export default function CircularIndeterminate() {
-//   return (
-//     <Box sx={{ display: "flex", marginLeft: "10px" }}>
-//       <CircularProgress size={22} className="!text-white" />
-//     </Box>
-//   );
-// }
+export default function CircularIndeterminate() {
+  return (
+    <Box sx={{ display: "flex", marginLeft: "10px" }}>
+      <CircularProgress size={22} className="!text-white" />
+    </Box>
+  );
+}
