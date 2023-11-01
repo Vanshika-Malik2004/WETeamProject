eea60b12c752f8361fbc4098a894985c8c789f14
Vanshika-Malik2004 <malikvanshika2004@gmail.com>
2023-11-01 20:36:47 +0530

    added authContext file that contails functions required for user authentication and EventContext that stores info about functions required for fetching data

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,71 +1,71 @@
-// import { createContext, useState, useEffect } from "react";
-// import nProgress from "nprogress";
-// import { auth, db, storage } from "../utils/dbConfig";
-// import firebase from "firebase";
+import { createContext, useState, useEffect } from "react";
+import nProgress from "nprogress";
+import { auth, db, storage } from "../utils/dbConfig";
+import firebase from "firebase";
 
-// export const AuthContext = createContext(null);
+export const AuthContext = createContext(null);
 
-// export const AuthProvider = ({ children }) => {
-//   const [fetchingUser, setFetchingUser] = useState(true);
-//   const [currentUser, setCurrentUser] = useState(null);
-//   const [isLoading, setIsLoading] = useState(false);
-//   // const [authError, setAuthError] = useState("");
+export const AuthProvider = ({ children }) => {
+  const [fetchingUser, setFetchingUser] = useState(true);
+  const [currentUser, setCurrentUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  // const [authError, setAuthError] = useState("");
 
-//   useEffect(() => {
-//     auth.onAuthStateChanged(async (user) => {
-//       if (user) {
-//         const res = await db.collection("users").doc(user.uid).get();
-//         if (!res.exists) {
-//           await db.collection("users").doc(user.uid).set({
-//             userId: user.uid,
-//             name: user.displayName,
-//             profilePhoto: user.photoURL,
-//             email: user.email,
-//           });
-//         }
-//         setCurrentUser({
-//           userId: user.uid,
-//           name: user.displayName,
-//           profilePhoto: user.photoURL,
-//           email: user.email,
-//         });
-//       } else {
-//         setCurrentUser(null);
-//       }
-//       setFetchingUser(false);
-//     });
-//   }, []);
+  useEffect(() => {
+    auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        const res = await db.collection("users").doc(user.uid).get();
+        if (!res.exists) {
+          await db.collection("users").doc(user.uid).set({
+            userId: user.uid,
+            name: user.displayName,
+            profilePhoto: user.photoURL,
+            email: user.email,
+          });
+        }
+        setCurrentUser({
+          userId: user.uid,
+          name: user.displayName,
+          profilePhoto: user.photoURL,
+          email: user.email,
+        });
+      } else {
+        setCurrentUser(null);
+      }
+      setFetchingUser(false);
+    });
+  }, []);
 
-//   const googleAuth = async () => {
-//     setIsLoading(true);
-//     nProgress.start();
-//     let provider = new firebase.auth.GoogleAuthProvider();
+  const googleAuth = async () => {
+    setIsLoading(true);
+    nProgress.start();
+    let provider = new firebase.auth.GoogleAuthProvider();
 
-//     try {
-//       await auth.signInWithPopup(provider);
-//     } catch (err) {
-//       setAuthError(err.message);
-//     }
+    try {
+      await auth.signInWithPopup(provider);
+    } catch (err) {
+      setAuthError(err.message);
+    }
 
-//     nProgress.done();
-//     setIsLoading(false);
-//   };
+    nProgress.done();
+    setIsLoading(false);
+  };
 
-//   const logout = async () => {
-//     await auth.signOut();
-//   };
+  const logout = async () => {
+    await auth.signOut();
+  };
 
-//   return (
-//     <AuthContext.Provider
-//       value={{
-//         googleAuth,
-//         isLoading,
-//         currentUser,
-//         authError,
-//         logout,
-//       }}
-//     >
-//       {!fetchingUser && children}
-//     </AuthContext.Provider>
-//   );
-// };
+  return (
+    <AuthContext.Provider
+      value={{
+        googleAuth,
+        isLoading,
+        currentUser,
+        authError,
+        logout,
+      }}
+    >
+      {!fetchingUser && children}
+    </AuthContext.Provider>
+  );
+};
--- src/contexts/EventContext.js
+++ src/contexts/EventContext.js
@@ -1,183 +1,183 @@
-// import { createContext, useState, useContext, useEffect } from "react";
-// import { AuthContext } from "./AuthContext";
-// import nProgress from "nprogress";
-// import { db } from "../utils/dbConfig";
-// export const EventContext = createContext(null);
+import { createContext, useState, useContext, useEffect } from "react";
+import { AuthContext } from "./AuthContext";
+import nProgress from "nprogress";
+import { db } from "../utils/dbConfig";
+export const EventContext = createContext(null);
 
-// export const EventProvider = ({ children }) => {
-//   const [isFetchingEvents, setIsFetchingEvents] = useState(false);
-//   const [isLoading, setIsLoading] = useState(false);
-//   const [events, setEvents] = useState(null);
-//   const [eventBookings, setEventBookings] = useState(null);
-//   const [data, setData] = useState(null);
-//   const { currentUser } = useContext(AuthContext);
+export const EventProvider = ({ children }) => {
+  const [isFetchingEvents, setIsFetchingEvents] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [events, setEvents] = useState(null);
+  const [eventBookings, setEventBookings] = useState(null);
+  const [data, setData] = useState(null);
+  const { currentUser } = useContext(AuthContext);
 
-//   useEffect(() => {
-//     if (currentUser) {
-//       fetchEvents();
-//     }
-//   }, [currentUser]);
-//   const fetchAllEducators = async () => {
-//     const res = await db.collection("users").get();
-//     const userData = [];
-//     res.docs.map((u) => {
-//       return userData.push({ ...u.data() });
-//     });
-//     return userData;
-//   };
+  useEffect(() => {
+    if (currentUser) {
+      fetchEvents();
+    }
+  }, [currentUser]);
+  const fetchAllEducators = async () => {
+    const res = await db.collection("users").get();
+    const userData = [];
+    res.docs.map((u) => {
+      return userData.push({ ...u.data() });
+    });
+    return userData;
+  };
 
-//   const fetchEducator = async (userId) => {
-//     let Eventsdata = [];
-//     const res = await db
-//       .collection("users")
-//       .doc(userId)
-//       .collection("events")
-//       .orderBy("timeStamp", "desc")
-//       .get();
-//     res.docs.map((event) => {
-//       Eventsdata.push({ ...event.data(), eventId: event.id });
-//     });
-//     return Eventsdata;
-//   };
-//   const fetchEducatorInfo = async (userId) => {
-//     const info = [];
-//     const res = (await db.collection("users").doc(userId).get()).data();
-//     console.log(res);
-//     return res;
-//   };
-//   const fetchEvents = async () => {
-//     setIsFetchingEvents(true);
-//     nProgress.start();
-//     let data = [];
-//     try {
-//       const res = await db
-//         .collection("users")
-//         .doc(currentUser.userId)
-//         .collection("events")
-//         .orderBy("timeStamp", "desc")
-//         .get();
+  const fetchEducator = async (userId) => {
+    let Eventsdata = [];
+    const res = await db
+      .collection("users")
+      .doc(userId)
+      .collection("events")
+      .orderBy("timeStamp", "desc")
+      .get();
+    res.docs.map((event) => {
+      Eventsdata.push({ ...event.data(), eventId: event.id });
+    });
+    return Eventsdata;
+  };
+  const fetchEducatorInfo = async (userId) => {
+    const info = [];
+    const res = (await db.collection("users").doc(userId).get()).data();
+    console.log(res);
+    return res;
+  };
+  const fetchEvents = async () => {
+    setIsFetchingEvents(true);
+    nProgress.start();
+    let data = [];
+    try {
+      const res = await db
+        .collection("users")
+        .doc(currentUser.userId)
+        .collection("events")
+        .orderBy("timeStamp", "desc")
+        .get();
 
-//       res.docs.map((event) => {
-//         data.push({ ...event.data(), eventId: event.id });
-//       });
+      res.docs.map((event) => {
+        data.push({ ...event.data(), eventId: event.id });
+      });
 
-//       setEvents(data);
-//     } catch (err) {
-//       console.log(err.message);
-//     }
-//     nProgress.done();
-//     setIsFetchingEvents(false);
-//   };
+      setEvents(data);
+    } catch (err) {
+      console.log(err.message);
+    }
+    nProgress.done();
+    setIsFetchingEvents(false);
+  };
 
-//   const fetchEventBookings = async (eventId) => {
-//     nProgress.start();
-//     let error;
-//     let data = [];
-//     try {
-//       const res = await db
-//         .collection("users")
-//         .doc(currentUser.userId)
-//         .collection("events")
-//         .doc(eventId)
-//         .collection("bookings")
-//         .get();
+  const fetchEventBookings = async (eventId) => {
+    nProgress.start();
+    let error;
+    let data = [];
+    try {
+      const res = await db
+        .collection("users")
+        .doc(currentUser.userId)
+        .collection("events")
+        .doc(eventId)
+        .collection("bookings")
+        .get();
 
-//       res.docs.map((booking) => {
-//         data.push({ ...booking.data(), bookingId: booking.id });
-//       });
+      res.docs.map((booking) => {
+        data.push({ ...booking.data(), bookingId: booking.id });
+      });
 
-//       setEventBookings(data);
+      setEventBookings(data);
 
-//       setEventBookings(data);
-//     } catch (err) {
-//       console.log("err");
-//       error = err.message;
-//     }
-//     nProgress.done();
-//     return {
-//       error: error ? error : null,
-//     };
-//   };
+      setEventBookings(data);
+    } catch (err) {
+      console.log("err");
+      error = err.message;
+    }
+    nProgress.done();
+    return {
+      error: error ? error : null,
+    };
+  };
 
-//   const createEvent = async (eventData) => {
-//     setIsLoading(true);
-//     nProgress.start();
-//     let error;
-//     try {
-//       await db
-//         .collection("users")
-//         .doc(currentUser.userId)
-//         .collection("events")
-//         .add(eventData);
-//     } catch (err) {
-//       error = err.message;
-//     }
-//     fetchEvents();
-//     nProgress.done();
-//     setIsLoading(false);
-//     return {
-//       error: error ? error : null,
-//     };
-//   };
+  const createEvent = async (eventData) => {
+    setIsLoading(true);
+    nProgress.start();
+    let error;
+    try {
+      await db
+        .collection("users")
+        .doc(currentUser.userId)
+        .collection("events")
+        .add(eventData);
+    } catch (err) {
+      error = err.message;
+    }
+    fetchEvents();
+    nProgress.done();
+    setIsLoading(false);
+    return {
+      error: error ? error : null,
+    };
+  };
 
-//   const deleteEvent = async (eventId) => {
-//     setIsLoading(true);
-//     nProgress.start();
-//     await db
-//       .collection("users")
-//       .doc(currentUser.userId)
-//       .collection("events")
-//       .doc(eventId)
-//       .delete();
-//     await fetchEvents();
-//     nProgress.done();
-//     setIsLoading(false);
-//   };
+  const deleteEvent = async (eventId) => {
+    setIsLoading(true);
+    nProgress.start();
+    await db
+      .collection("users")
+      .doc(currentUser.userId)
+      .collection("events")
+      .doc(eventId)
+      .delete();
+    await fetchEvents();
+    nProgress.done();
+    setIsLoading(false);
+  };
 
-//   const fetchEventData = async (userId, eventId) => {
-//     nProgress.start();
-//     let error;
+  const fetchEventData = async (userId, eventId) => {
+    nProgress.start();
+    let error;
 
-//     try {
-//       const res1 = await db.collection("users").doc(userId).get();
+    try {
+      const res1 = await db.collection("users").doc(userId).get();
 
-//       const res2 = await db
-//         .collection("users")
-//         .doc(userId)
-//         .collection("events")
-//         .doc(eventId)
-//         .get();
+      const res2 = await db
+        .collection("users")
+        .doc(userId)
+        .collection("events")
+        .doc(eventId)
+        .get();
 
-//       setData({ ...res1.data(), ...res2.data() });
-//       console.log(res1, res2);
-//     } catch (err) {
-//       console.log(err.message);
-//     }
-//     nProgress.done();
-//     return {
-//       error: error ? error : null,
-//     };
-//   };
+      setData({ ...res1.data(), ...res2.data() });
+      console.log(res1, res2);
+    } catch (err) {
+      console.log(err.message);
+    }
+    nProgress.done();
+    return {
+      error: error ? error : null,
+    };
+  };
 
-//   return (
-//     <EventContext.Provider
-//       value={{
-//         isFetchingEvents,
-//         isLoading,
-//         createEvent,
-//         fetchEvents,
-//         events,
-//         deleteEvent,
-//         eventBookings,
-//         fetchEventBookings,
-//         fetchEventData,
-//         fetchAllEducators,
-//         fetchEducator,
-//         fetchEducatorInfo,
-//         data,
-//       }}
-//     >
-//       {!isLoading && children}
-//     </EventContext.Provider>
-//   );
-// };
+  return (
+    <EventContext.Provider
+      value={{
+        isFetchingEvents,
+        isLoading,
+        createEvent,
+        fetchEvents,
+        events,
+        deleteEvent,
+        eventBookings,
+        fetchEventBookings,
+        fetchEventData,
+        fetchAllEducators,
+        fetchEducator,
+        fetchEducatorInfo,
+        data,
+      }}
+    >
+      {!isLoading && children}
+    </EventContext.Provider>
+  );
+};
